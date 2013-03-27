f9c8b0f0d7e803ba505c91523540eafb2e937be0
Timothy Overly <timothy.overly@gmail.com>
2013-03-27 17:34:01 -0400

    Made obnoxious variable names to make sure we don't get odd interferance

--- gitstatus.sh
+++ gitstatus.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 # tputcolors
 
-LOGSTRING=log.log
-exec 2>$LOGSTRING
+GITSTATUSLOGSTRING=log.log
+exec 2>$GITSTATUSLOGSTRING
 
 for D in `find . -maxdepth 1 -type d`
 do
   if [[ "$D" != "." ]]; then
     cd "$D"
-    STATUS=`git status | grep "nothing to commit"`
-    ERROR=`git status | sed -n 1p`
-    ISREPO="false"
-    if [[ "$ERROR" != "" ]]; then
-      ISREPO="true"
+    GITSTATUSSTATUS=`git status | grep "nothing to commit"`
+    GITSTATUSERROR=`git status | sed -n 1p`
+    GITSTATUSISREPO="false"
+    if [[ "$GITSTATUSERROR" != "" ]]; then
+      GITSTATUSISREPO="true"
     fi
-    BRANCH=`git status | sed -n 1p`
-    BRANCH=${BRANCH:12}
-    if [[ "$ISREPO" = "true" ]]; then
+    GITSTATUSBRANCH=`git status | sed -n 1p`
+    GITSTATUSBRANCH=${GITSTATUSBRANCH:12}
+    if [[ "$GITSTATUSISREPO" = "true" ]]; then
         #statements
-      if [[ "$STATUS" != "" ]]; then
+      if [[ "$GITSTATUSSTATUS" != "" ]]; then
         #echo " $(tput setaf 1)Text$(tput sgr0)"
-        STATUS="$(tput setaf 2)✓$(tput sgr0)"
+        GITSTATUSSTATUS="$(tput setaf 2)✓$(tput sgr0)"
       else  
-        STATUS="$(tput setaf 1)✗$(tput sgr0)"
+        GITSTATUSSTATUS="$(tput setaf 1)✗$(tput sgr0)"
       fi
-      echo "$(tput setaf 6)$D$(tput sgr0) - $(tput setaf 5)$BRANCH$(tput sgr0): $STATUS"
+      echo "$(tput setaf 6)$D$(tput sgr0) - $(tput setaf 5)$GITSTATUSBRANCH$(tput sgr0): $GITSTATUSSTATUS"
     fi
     cd ..
     rm -f log.log
